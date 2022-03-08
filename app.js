051f99f2b54e92a19ca5a44a462a414469cf9ee0
Jatin <jatin8085vashisht@gmail.com>
2022-03-08 15:38:31 +0530

    upload files to server with multer

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+// set storage engine
+const storage = multer.diskStorage(
+    {
+        destination: './public/uploads',
+        // function takes three things,
+        // request (req)
+        // actuale file (file)
+        // callback
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+            );
+        }
+    }
+);
+
+// initialize upload variable
+const upload = multer({
+    storage: storage
+}).single('myImage'); // myImage is the field name 
+// you can also upload more than 1 images just use array instead of single
+
+// init app
+const app = express();
+
+const port = 3000;
+
+// public folder
+app.use(express.static('./public'));
+
+app.get('/', (res, req) => {
+    req.send('Home Page')
+})
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) =>{
+        if(err){
+            res.send("Unable to upload file,")
+        }else{
+            console.log(req.file)
+            res.send("file uploaded")
+        }
+    });
+});
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`))
